faaca6179df316e9e95c28b88fb192f7dd87f008
Nandini Garg <55273422+nandiniigarg@users.noreply.github.com>
2021-02-19 18:44:36 +0530

    Add files via upload

--- playdate_main.py
+++ playdate_main.py
@@ -1,6 +1,5 @@
-from PIL import Image
-import pandas as pd
-#
+#from PIL import Image
+from tkinter import messagebox
 
 class doggie:
 
@@ -12,15 +11,15 @@ class doggie:
         self.breed = breed
         self.medical_history = medical_history
 
-class article:
+# class article:
     
-    def __init__(self, origin, popu, looks, temper, health, diet):
-        self.origin = origin
-        self.popu = popu
-        self.looks = looks
-        self.temper = temper
-        self.health = health
-        self.diet = diet
+#     def __init__(self, origin, popu, looks, temper, health, diet):
+#         self.origin = origin
+#         self.popu = popu
+#         self.looks = looks
+#         self.temper = temper
+#         self.health = health
+#         self.diet = diet
     
 
 
@@ -54,39 +53,40 @@ def couple(a):
                         if a.breed == i.breed:
                             match_email.append(i.email)
                             match_name.append(i.name)
-        return match_name, match_email
+        messagebox.showinfo('You have a match!', ("You have matched with:\n", match_name,"\nYou can contact them on the following email(s)\n", match_email))
 
-print(couple(dog4))
+couple(dog4)
 
-beagle = article("Great Britain", 
-"The association of Masters of Beagles called as Beagle Club in the year 1896 held their first show at Peterborough in the United Kingdom.\nThe regular showing of the breed brought limelight to them and slowly their popularity started increasing.\nBut still they became more popular in the United States and Canada than in their native England.", 
-"The general appearance of the beagle is like a miniature Foxhound,legs shorter as compared to full body proportion.\nThey are mostly between 13 to 16 inches high and weigh between 18 to 35 lb ,\nfemales being slightly smaller than the males.",
-"They are very friendly and curious in nature.\nThey are neither aggressive nor timid,\nthey enjoy company of people a lot even with strangers if treated well they easily become friendly with them. ", 
-"Beagles are prone to epilepsy, dwarfism is also seen in most of the breed.\nThey are considered chondrodystrophic breeds which are prone to types of disk diseases.\nHip dysplasia is also a common health issue found in some of the larger breeds.\nSome of the above mentioned issues can be cured with medications as well but their proper care is needed.",
-"Beagles can eat eggs, fish, most of the meats, cheese, peanut butter, popcorn, tuna, yogurt wheat and most of the grains.\nAvoid feeding them avocado, mustard seeds, coffee, Raisins, tea, sugarless gum anything that contains xylitol should be avoided.")
+# beagle = article("Great Britain", 
+# "The association of Masters of Beagles called as Beagle Club in the year 1896 held their first show at Peterborough in the United Kingdom.\nThe regular showing of the breed brought limelight to them and slowly their popularity started increasing.\nBut still they became more popular in the United States and Canada than in their native England.", 
+# "The general appearance of the beagle is like a miniature Foxhound,legs shorter as compared to full body proportion.\nThey are mostly between 13 to 16 inches high and weigh between 18 to 35 lb ,\nfemales being slightly smaller than the males.",
+# "They are very friendly and curious in nature.\nThey are neither aggressive nor timid,\nthey enjoy company of people a lot even with strangers if treated well they easily become friendly with them. ", 
+# "Beagles are prone to epilepsy, dwarfism is also seen in most of the breed.\nThey are considered chondrodystrophic breeds which are prone to types of disk diseases.\nHip dysplasia is also a common health issue found in some of the larger breeds.\nSome of the above mentioned issues can be cured with medications as well but their proper care is needed.",
+# "Beagles can eat eggs, fish, most of the meats, cheese, peanut butter, popcorn, tuna, yogurt wheat and most of the grains.\nAvoid feeding them avocado, mustard seeds, coffee, Raisins, tea, sugarless gum anything that contains xylitol should be avoided.")
 
-def personal_blog(a):
-        week = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday']
-        day = input("What day is it today? \n")
-        br = a.breed
-        if day == week[0]:
-            print("Your dog is from:",br.origin)
-        elif day == week[1]:
-            print("Your dog is popular for many reasons but here is one of them:\n",br.popu)
-        elif day == week[2]:
-            print("Here are some facts about your dog's appearance:\n",br.looks)
-        elif day == week[3]:
-            print("Some facts about your dog's temper:\n",br.temper)
-        elif day == week[4]:
-            print("How about some health facts about your loyal friend?\n",br.health)
-        elif day == week[5]:
-            print("Here are some dietary facts about your dog:\n",br.diet)
-        elif day == week[6]:
-            print("Here's a puppy picture to make your sunday! ;)") 
-            im = Image.open("/Users/nandinigarg/Desktop/beagle.jpg")
-            im.show()       
+# def personal_blog(a):
+#         week = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday']
+#         day = input("What day is it today? \n")
+#         br = a.breed
+#         if day == week[0]:
+#             print("Your dog is from:",br.origin)
+#         elif day == week[1]:
+#             print("Your dog is popular for many reasons but here is one of them:\n",br.popu)
+#         elif day == week[2]:
+#             print("Here are some facts about your dog's appearance:\n",br.looks)
+#         elif day == week[3]:
+#             print("Some facts about your dog's temper:\n",br.temper)
+#         elif day == week[4]:
+#             print("How about some health facts about your loyal friend?\n",br.health)
+#         elif day == week[5]:
+#             print("Here are some dietary facts about your dog:\n",br.diet)
+#         elif day == week[6]:
+#             print("Here's a puppy picture to make your sunday! ;)") 
+#             im = Image.open("/Users/nandinigarg/Desktop/beagle.jpg")
+#             im.show()       
             
 #personal_blog(dog1)
+
 
 #personal profile
 
@@ -94,28 +94,6 @@ def personal_blog(a):
 
 
 #CoviCheck
-
-url = 'https://www.mohfw.gov.in/'
-
-web_content = requests.get(url).content
-
-soup = BeautifulSoup(web_content, "html.parser")
-
-extract_contents = lambda row: [x.text.replace('\n', '') for x in row]
-
-stats = [] 
-all_rows = soup.find_all('tr')
-for row in all_rows:
-    stat = extract_contents(row.find_all('td')) 
-
-    if len(stat) == 5:
-        stats.append(stat)
-
-new_cols = ["Sr.No", "States/UT","Confirmed","Recovered","Deceased"]
-state_data = pd.DataFrame(data = stats, columns = new_cols)
-state_data['Confirmed'] = state_data['Confirmed'].map(int)
-state_data['Recovered'] = state_data['Recovered'].map(int)
-state_data['Deceased']  = state_data['Deceased'].map(int)
 
 #print(state_data)
 
